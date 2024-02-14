92f8cf92cfc54dd2f884b1696ec50290bb85d3a9
chonlasak <areasm456@gmail.com>
2024-02-14 12:47:44 +0700

    env change

--- src/App.js
+++ src/App.js
@@ -3,137 +3,108 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import { Header } from './components/Header'
 import { DisplayBoard } from './components/DisplayBoard'
-
 //----------------
 import { Movies } from './components/Movies'
 import CreateMovie from './components/CreateMovie'
-import SearchMovie from './components/SearchMovie'
-import { getAllMovies, createMovie, searchMovie } from './services/MovieService'
+import { getAllMovies, createMovie } from './services/MovieService'
+
+//เพิ่มใหม่
+import SearchPage from './components/SearchPage';
+
 
 function App() {
 
+  const [user, setUser] = useState({})
+  const [users, setUsers] = useState([])
+  const [numberOfUsers, setNumberOfUsers] = useState(0)
+
   //--------------
-  const [search_data, setSearch_data] = useState({})
   const [movie, setMovie] = useState({})
   const [movies, setMovies] = useState([])
   const [numberOfMovies, setNumberOfMovies] = useState(0)
-
-  const movieCreate = (e) => {
-
-    createMovie(movie)
-      .then(response => {
-        console.log(response);
-        setNumberOfMovies(numberOfMovies + 1)
-      });
-
-    fetchAllMovies(); /* */
-  }
+  const [searchResults, setSearchResults] = useState([]);
 
 
-  const movieSearch = (e) => {
+const movieCreate = (e) => {
 
-    searchMovie("jok")
-      .then(movies => {
-        console.log(movies.data);
-        setMovies(movies.data);
-        setNumberOfMovies(movies.data.length);
-
-      });
-
-  }
-
-  const fetchAllMovies = () => {
-    getAllMovies()
-      .then(movies => {
-        console.log(movies);
-        setMovies(movies);
-        setNumberOfMovies(movies.length);
-      });
-  }
+  createMovie(movie)
+    .then(response => {
+      console.log(response);
+      setNumberOfMovies(numberOfMovies+1)
+  });
+}
 
 
-  useEffect(() => {
+const fetchAllMovies = () => {
+  getAllMovies()
+    .then(movies => {
+      console.log(movies)
+      setMovies(movies);
+      setNumberOfMovies(movies.length)
+      setSearchResults([]);
+    });
+}
+
+useEffect(() => {
 
     getAllMovies()
-      .then(movies => {
-        console.log(movies)
-        setMovies(movies);
-        setNumberOfMovies(movies.length)
-      });
-      
+    .then(movies => {
+      console.log(movies)
+      setMovies(movies);
+      setNumberOfMovies(movies.length)
+    });
 
-  }, [])
+}, [])
 
   const onChangeForm = (e) => {
     if (e.target.name === 'title') {
-      movie.title = e.target.value;
+        movie.title = e.target.value;
     } else if (e.target.name === 'genre') {
-      movie.genre = e.target.value;
+        movie.genre = e.target.value;
     } else if (e.target.name === 'director') {
-      movie.director = e.target.value;
+        movie.director = e.target.value;
     } else if (e.target.name === 'release_year') {
-      movie.release_year = e.target.value;
+        movie.release_year = e.target.value;
     }
     setMovie(movie)
-  }
+}
 
-
-  const onChangeForm2 = (e) => {
-    if (e.target.name === 'search_text') {
-      search_data.search_text = e.target.value;
-    } 
-    setSearch_data(search_data)
-  }
-
-
-  return (
-    <div className="App">
+   return (
+    <div className="App" style={{backgroundImage: 'url("/images/car.jpg")', backgroundSize: 'cover', minHeight: '100vh'}}>
       <Header></Header>
       <div className="container mrgnbtm">
         <div className="row">
           <div className="col-md-8">
             <CreateMovie
-              // user={user}
-              movie={movie}
+              Movies={Movies}
               onChangeForm={onChangeForm}
               createMovie={movieCreate}
             >
             </CreateMovie>
+            <div className="col-md-8">
+            <SearchPage
+              setMovies={setMovies}
+              setNumberOfMovies={setNumberOfMovies}
+              setSearchResults={setSearchResults}
+            />
+            </div>
           </div>
           <div className="col-md-4">
-            <DisplayBoard
-              numberOfMovies={numberOfMovies}
-              getAllMovies={fetchAllMovies}
-            >
-            </DisplayBoard>
+                  <DisplayBoard
+                    numberOfMovies={numberOfMovies}
+                    getAllMovies={fetchAllMovies}
+                  >
+                  </DisplayBoard> 
           </div>
         </div>
       </div>
       <div className="row mrgnbtm">
-
-          <SearchMovie
-              // user={user}
-              movie={movie}
-              onChangeForm={onChangeForm}
-              searchMovie={movieSearch}
-            >
-          </SearchMovie>
-
-        {/* <SearchBoard
-          // user={user}
-          search_data={search_data}
-          onChangeForm2={onChangeForm2}
-          searchMovies={searchMovie}
-        >
-        </SearchBoard> */}
-
-      </div>
-      <div className="row mrgnbtm">
-        <Movies movies={movies}></Movies>
-      </div>
-
+        <Movies movies={searchResults.length > 0 ? searchResults : movies}></Movies>
+      </div>   
     </div>
   );
 }
 
 export default App;
+
+ 
--- src/services/MovieService.js
+++ src/services/MovieService.js
@@ -1,56 +1,44 @@
-import { env } from "../env.js";
-import { apiConfig } from "../apiConfig.js";
-
-export async function searchMovie(search_data) {
-  try {
-    console.log("search_data: " + search_data);
-    const response = await fetch(
-      apiConfig[env].API_URL + "/api/movie/search?search_text=" + search_data,
-      {
-        headers: {
-          Authorization: `Bearer ${apiConfig[env].bearer_token}`,
-        },
-      }
-    );
-
-    return await response.json(); //***
-
-    // console.log('Search Movie response: ' + await response);
-  } catch (error) {
-    return [];
-  }
-}
-
+const env = require('../components/env');
+const config = require('../components/apiConfig')[env];
+const token = config.bearer_token;
 export async function getAllMovies() {
-  try {
-    //const response = await fetch('http://localhost:4000/api/movie/all',
-    const response = await fetch(apiConfig[env].API_URL + "/api/movie/all", {
-      headers: {
-        Authorization: `Bearer ${apiConfig[env].bearer_token}`,
-      },
-    });
-    return await response.json();
-  } catch (error) {
-    return [];
-  }
+    try {
+        const response = await fetch(config.API_URL +'/api/movie/all', {
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        });
+        return await response.json();
+    } catch (error) {
+        return [];
+    }
 }
 
 export async function createMovie(data) {
-  try {
-    const response = await fetch(apiConfig[env].API_URL + "/api/movie/insert", {
-      //mode: 'no-cors',
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${apiConfig[env].bearer_token}`,
-      },
-      body: JSON.stringify(data),
-    });
-    console.log("Create Movie response: " + (await response));
+    try {
+        const response = await fetch(config.API_URL +'/api/movie/insert', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`
+            },
+            body: JSON.stringify(data)
+        });
+        return await response.json();
+    } catch (error) {
+        return { error: 'Failed to create movie' };
+    }
+}
 
-    // return  response.json();
-    return response;
-  } catch (error) {
-    return [];
-  }
+export async function getMovieSearch(search_text) {
+    try {
+        const response = await fetch(`${config.API_URL}/api/movie/search?search_text=${search_text}`, {
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }          
+        });
+        return await response.json();
+    } catch (error) {
+        return [];
+    }
 }
